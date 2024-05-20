741fa9500b071e7247ec99a69a3d1982643d3b35
shubhujais15 <shubhujaisedu15@gmail.com>
2024-05-20 21:54:47 +0530

    Added rest api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,2 @@
 export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.96340&lng=77.58550&restaurantId="
+export const REST_API = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.96340&lng=77.58550&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
