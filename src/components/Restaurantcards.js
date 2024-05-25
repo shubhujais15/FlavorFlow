5241f593c83d4c28403886ba43659557b9d9efc2
shubhujais15 <shubhujaisedu15@gmail.com>
2024-05-26 00:58:25 +0530

    Added RestaurantCategory

--- src/components/Restaurantcards.js
+++ src/components/Restaurantcards.js
@@ -2,14 +2,24 @@ const Restaurantcards = (props) => {
     const {resData} = props;
     const {cloudinaryImageId,name,cuisines,locality,avgRating} = resData?.info;
     return(
-      <div className="restaurantCardItems">
-        <img className="restPic" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+resData.info.cloudinaryImageId}></img>
-        <h5>{name}</h5>
-        <p>{cuisines.join(", ")}</p>   
-        <p>{locality}</p>
-        <p>{avgRating}</p>
+      <div className="mx-8 my-6 p-2 h-[305px] w-[220px] rounded-lg bg-slate-50 transition-transform duration-500 transform scale-100 hover:scale-105">
+        <img className="h-32 w-48 mx-1.5 mt-2 rounded-lg" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+cloudinaryImageId}></img>
+        <h5 className="ml-1.5 mt-0.5 text-sm font-bold">{name}</h5>
+        <p className="ml-2.5 mt-1 text-sm">{cuisines.join(", ")}</p>   
+        <p className="ml-2.5 text-sm">{locality}</p>
+        <p className="ml-2.5 text-sm">{avgRating}</p>
       </div>
     )
-  }
+  };
 
+  // export const  withPromotedLabel =(Restaurantcards) => {
+  //   return(props) => {
+  //     return(
+  //     <div>
+  //     <label>Promoted</label>
+  //     <Restaurantcards {...props} />
+  //     </div>
+  //   )
+  // }}
+   
   export default Restaurantcards;
