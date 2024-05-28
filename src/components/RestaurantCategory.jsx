95564bdccdf70075c2e084818c60e8ca4ea1c5af
shubhujais15 <shubhujaisedu15@gmail.com>
2024-05-28 08:25:09 +0530

    Accordion head & body

---
+++ src/components/RestaurantCategory.jsx
@@ -0,0 +1,32 @@
+import ItemList from "./ItemList";
+import { useState } from "react";
+
+const RestaurantCategory = ({data,showItems,setShowIndex}) => {
+    console.log(data);
+    // console.log(showItems)
+    // const [showItems, setshowItems] = useState(false)
+   
+
+    const handleClick = () => {
+        // setshowItems(!showItems)
+        setShowIndex();
+        console.log("Clicked")
+    }
+
+    return(
+        <div>
+        <div className="w-6/12 mx-auto my-6 shadow-xl bg-gray-100 p-4 rounded-lg">
+                    {/* head */}
+        <div className="flex justify-between cursor-pointer"
+        onClick={handleClick}>
+
+            <span className="font-bold">{data.title} ({data.itemCards.length})</span>
+            <span>⬇️</span>
+            {/* Accordion Body */}
+            </div>
+            {showItems && <ItemList key={data?.itemCards?.card?.info?.id} items={data.itemCards} />}
+        </div>
+        </div>
+    )
+}
+export default RestaurantCategory;
