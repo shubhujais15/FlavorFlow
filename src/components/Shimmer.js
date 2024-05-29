05849db86e66366f6268fd4bc6e9420a8c700dc7
shubhujais15 <shubhujaisedu15@gmail.com>
2024-05-30 00:25:29 +0530

    Adding tailwind

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,29 +1,30 @@
 const Shimmer = () => {
     return(
        
-     <div  className="bodyContainer">
-      <div className="searchBar">
-        <input type="text" className="inputBox"></input>
-        <button className="searchBtn" >Search</button>
-        <button className="filt-btn">
+     <div  className="bg-slate-400 mt-2.5 m-0.5 rounded-xl">
+      <div className="flex justify-center m-1.5 p-3">
+        <input type="text" className="mt-3 h-10 w-96 rounded-full text-base indent-3.5 outline-none"></input>
+        <button className="bg-yellow-100 mx-3 mt-3 h-9 w-20 rounded-full" >Search</button>
+        <button className="bg-yellow-100 mt-3 mx-3 h-9 w-48 rounded-full">
           Top Rated Restaurant
-        </button>
+          </button>
       </div>
-      <div className="final-cards">
-        <div className="shimmer-cards"><h3 className="shimmerText">Loading...</h3></div>
-        <div className="shimmer-cards"><h3 className="shimmerText">Loading...</h3></div>
-        <div className="shimmer-cards"><h3 className="shimmerText">Loading...</h3></div>
-        <div className="shimmer-cards"><h3 className="shimmerText">Loading...</h3></div>
-        <div className="shimmer-cards"><h3 className="shimmerText">Loading...</h3></div>
-        <div className="shimmer-cards"><h3 className="shimmerText">Loading...</h3></div>
-        <div className="shimmer-cards"><h3 className="shimmerText">Loading...</h3></div>
-        <div className="shimmer-cards"><h3 className="shimmerText">Loading...</h3></div>
-        <div className="shimmer-cards"><h3 className="shimmerText">Loading...</h3></div>
-        <div className="shimmer-cards"><h3 className="shimmerText">Loading...</h3></div>
-        <div className="shimmer-cards"><h3 className="shimmerText">Loading...</h3></div>
-        <div className="shimmer-cards"><h3 className="shimmerText">Loading...</h3></div>
-        <div className="shimmer-cards"><h3 className="shimmerText">Loading...</h3></div>
-        <div className="shimmer-cards"><h3 className="shimmerText">Loading...</h3></div>
+      <div className="flex flex-wrap my-6 ml-12">
+        <div className="mx-8 my-6 p-2 h-[305px] w-[220px] rounded-lg bg-slate-200 hover:bg-slate-300 flex items-center justify-center"><h3 className="text-center">Loading...</h3></div>
+        <div className="mx-8 my-6 p-2 h-[305px] w-[220px] rounded-lg bg-slate-200 hover:bg-slate-300 flex items-center justify-center"><h3 className="text-center">Loading...</h3></div>
+        <div className="mx-8 my-6 p-2 h-[305px] w-[220px] rounded-lg bg-slate-200 hover:bg-slate-300 flex items-center justify-center"><h3 className="text-center">Loading...</h3></div>
+        <div className="mx-8 my-6 p-2 h-[305px] w-[220px] rounded-lg bg-slate-200 hover:bg-slate-300 flex items-center justify-center"><h3 className="text-center">Loading...</h3></div>
+        <div className="mx-8 my-6 p-2 h-[305px] w-[220px] rounded-lg bg-slate-200 hover:bg-slate-300 flex items-center justify-center"><h3 className="text-center">Loading...</h3></div>
+        <div className="mx-8 my-6 p-2 h-[305px] w-[220px] rounded-lg bg-slate-200 hover:bg-slate-300 flex items-center justify-center"><h3 className="text-center">Loading...</h3></div>
+        <div className="mx-8 my-6 p-2 h-[305px] w-[220px] rounded-lg bg-slate-200 hover:bg-slate-300 flex items-center justify-center"><h3 className="text-center">Loading...</h3></div>
+        <div className="mx-8 my-6 p-2 h-[305px] w-[220px] rounded-lg bg-slate-200 hover:bg-slate-300 flex items-center justify-center"><h3 className="text-center">Loading...</h3></div>
+        <div className="mx-8 my-6 p-2 h-[305px] w-[220px] rounded-lg bg-slate-200 hover:bg-slate-300 flex items-center justify-center"><h3 className="text-center">Loading...</h3></div>
+        <div className="mx-8 my-6 p-2 h-[305px] w-[220px] rounded-lg bg-slate-200 hover:bg-slate-300 flex items-center justify-center"><h3 className="text-center">Loading...</h3></div>
+        <div className="mx-8 my-6 p-2 h-[305px] w-[220px] rounded-lg bg-slate-200 hover:bg-slate-300 flex items-center justify-center"><h3 className="text-center">Loading...</h3></div>
+        <div className="mx-8 my-6 p-2 h-[305px] w-[220px] rounded-lg bg-slate-200 hover:bg-slate-300 flex items-center justify-center"><h3 className="text-center">Loading...</h3></div>
+        <div className="mx-8 my-6 p-2 h-[305px] w-[220px] rounded-lg bg-slate-200 hover:bg-slate-300 flex items-center justify-center"><h3 className="text-center">Loading...</h3></div>
+        <div className="mx-8 my-6 p-2 h-[305px] w-[220px] rounded-lg bg-slate-200 hover:bg-slate-300 flex items-center justify-center"><h3 className="text-center">Loading...</h3></div>
+        <div className="mx-8 my-6 p-2 h-[305px] w-[220px] rounded-lg bg-slate-200 hover:bg-slate-300 flex items-center justify-center"><h3 className="text-center">Loading...</h3></div>
         
       </div>
     </div>
