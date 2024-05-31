cbe29ae7e5cfb482adc644a3bbc9fd076af30861
shubhujais15 <shubhujaisedu15@gmail.com>
2024-05-31 19:18:34 +0530

    Intial commit

---
+++ src/components/ShimmerMenu.jsx
@@ -0,0 +1,9 @@
+const ShimmerMenu = () => {
+    return(
+        <div className="justify-center">
+            <h1 className="italic my-6 text-2xl text-center">Your Menu is Being Ready...😉</h1>
+
+        </div>
+    )
+}
+export default ShimmerMenu;
