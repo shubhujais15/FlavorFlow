b9b4e7aef35dc4ed8e1b4e7c5a6d84745285d4e4
shubhujais15 <shubhujaisedu15@gmail.com>
2024-06-04 01:42:00 +0530

    Added useSelector

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,14 @@
 import {Link} from 'react-router-dom';
 import useOnlineStatus from '../utils/useOnlineStatus';
+import { useSelector } from 'react-redux';
+
 
 const Header = () => {
   const onlineStatus = useOnlineStatus();
+
+  const cartItems = useSelector((store)=>store.cart.items)
+  console.log(cartItems)
+
   return(
     <div className="flex justify-between bg-pink-200 content-center shadow-xl m-0.5 rounded-xl">
       <div className="flex">
@@ -22,9 +28,12 @@ const Header = () => {
           <li className='text-lg mr-12'><Link to="./about">About</Link></li>
           <li className='text-lg mr-12'><Link to="./contact">Contact</Link></li> 
           <li className='text-lg mr-12'>
+            <div className='flex'>
             <Link to="./cart">
             <img className="w-11 h-11 rounded-full mb-1.5" src="https://static.vecteezy.com/system/resources/thumbnails/004/798/846/small/shopping-cart-logo-or-icon-design-vector.jpg"></img>
             </Link>
+            <div className='bg-black text-white w-5 h-5 rounded-full justify-center'><p className='text-sm text-center'>{cartItems.length}</p></div>
+            </div>
           </li>
         </ul>
       </div>
