6be33f8d59eebdd8861cc91814fb33136f7e40e7
shubhujais15 <shubhujaisedu15@gmail.com>
2024-06-04 01:44:19 +0530

    Update CSS

--- src/components/Body.js
+++ src/components/Body.js
@@ -11,11 +11,11 @@ const Body = () => {
   // It is a state variable that holds the list of restaurants, and  it's initialized with an resList data in useState hook.
   // let [ListofRestaurant , setListofRestaurant] = useState(resList);
 
-  let [ListofRestaurant , setListofRestaurant] = useState([]);
+  const [ListofRestaurant , setListofRestaurant] = useState([]);
   // console.log(ListofRestaurant);
 
   //  Creating a copy of ListofRestaurant which is used for filtering restaurant list without losting the original data
-  let [FilteredRestaurant , setFilteredRestaurant] = useState([]);
+  const [FilteredRestaurant , setFilteredRestaurant] = useState([]);
 
   // creating a useState to filter restaurant after searching some specific restuarants
   let [SearchText , setSearchText] = useState("");
@@ -40,7 +40,7 @@ const Body = () => {
   }  
 
     const onlineStatus = useOnlineStatus();
-    if(onlineStatus === false) return <h1>U are offline</h1>
+    if(onlineStatus === false) return <h1 className="justify-center text-center">U are offline</h1>
 
   // Conditional Rendering
   if(ListofRestaurant.length === 0){
@@ -52,13 +52,13 @@ const Body = () => {
     <div  className="bg-slate-400 mt-2.5 m-0.5 rounded-xl">
       <div className="flex justify-center m-1.5 p-3">
     {/* In value we pass the state variable name in which we have to set the value */}
-        <input type="text" className=" mt-3 h-10 w-96 rounded-full text-base indent-3.5 outline-none" value={SearchText} 
+        <input type="text" className=" mt-3 h-10 w-96 rounded-full text-base indent-3.5 outline-none focus:border-indigo-500 focus:ring-indigo-400 transition-colors duration-200 ease-in-out focus:ring-2" value={SearchText} 
         onChange={(e)=>{
           setSearchText(e.target.value);
         }}></input>
 
 
-        <button className="bg-yellow-100 mx-3 mt-3 h-9 w-20 rounded-full" 
+        <button className="bg-yellow-100 hover:bg-yellow-200 mx-3 mt-3 h-9 w-20 rounded-full hover:scale-105 active:scale-95 focus:outline-none" 
         onClick={()=>{
           const filteredRest = ListofRestaurant.filter(
             (res) => res.info.name.toLowerCase().includes(SearchText.toLowerCase())
@@ -71,7 +71,8 @@ const Body = () => {
           {/* setListofRestaurant(ListofRestaurant); */}
 
 
-        <button className=" bg-yellow-100 mt-3 mx-3 h-9 w-48 rounded-full" onClick={() => {
+        <button className=" bg-yellow-100 hover:bg-yellow-200 mt-3 mx-3 h-9 w-48 rounded-full hover:scale-105 active:scale-95 focus:outline-none" 
+        onClick={() => {
           const filteredList = ListofRestaurant.filter(
             (res) => res.info.avgRating > 4.3
           ); 
