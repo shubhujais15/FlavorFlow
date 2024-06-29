8f5b02ab4b8826e7de124c8bfb8e1cc9390f0200
shubhujais15 <shubhujaisedu15@gmail.com>
2024-06-29 11:21:48 +0530

    adding cors api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,3 @@
-export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.96340&lng=77.58550&restaurantId="
-export const REST_API = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.96340&lng=77.58550&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+export const CORS_PROXY = "https://flavorflow-backend.onrender.com/";
+export const MENU_API = CORS_PROXY + "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.96340&lng=77.58550&restaurantId=";
+export const REST_API = CORS_PROXY + "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.96340&lng=77.58550&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
