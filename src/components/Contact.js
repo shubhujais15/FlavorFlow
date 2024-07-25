7ddeead5932f6694237b256511a57022a86a2a3e
shubhujais15 <shubhujaisedu15@gmail.com>
2024-07-25 23:39:43 +0530

    added form action & method

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -21,13 +21,14 @@ const Contact = () => {
                         </div>
                         <div className="lg:w-1/2 md:w-2/3 mx-auto">
                             <div className="flex flex-wrap -m-2">
-                                <div className="p-2 w-1/2">
+                                <form action="https://getform.io/f/amdpyoob" method="POST" className="w-full">
+                                <div className="p-2 w-full">
                                     <div className="relative">
                                         <label htmlFor="name" className="leading-7 text-sm text-gray-600">Name</label>
                                         <input type="text" id="name" name="name" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
                                     </div>
                                 </div>
-                                <div className="p-2 w-1/2">
+                                <div className="p-2 w-full">
                                     <div className="relative">
                                         <label htmlFor="email" className="leading-7 text-sm text-gray-600">Email</label>
                                         <input type="email" id="email" name="email" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
@@ -42,6 +43,7 @@ const Contact = () => {
                                 <div className="p-2 w-full">
                                     <button className="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Send</button>
                                 </div>
+                                </form>
                                 <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
                                     <a href="mailto:shubhujais@gmail.com" className="text-indigo-500">shubhujais@gmail.com</a>
                                     <p className="leading-normal my-5">JSSATE Bangalore<br/>Kengeri, Main Road, 560060</p>
